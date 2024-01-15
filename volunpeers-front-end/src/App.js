30b314164ae436d2412c8e5a865e97f36407ead3
sofiamaipa <sofiamaipa@gmail.com>
2024-01-15 15:27:37 +0100

    Updated App.js file in order to add the functionality of multiple-pages to our site. I made it work for Profile page and left comments on how to add the other pages.

--- volunpeers-front-end/src/App.js
+++ volunpeers-front-end/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Profile from "./Profile";
+/*  Add one following lines above, Make the appropriate name changes to match your page.
+import Home from "./pages";
+import About from "./pages/about";
+import SignUp from "./pages/signup";
+import Contact from "./pages/contact";*/
+
+/* Then add one following lines inside <Routes> , with the names changed according to your files.
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/sign-up" element={<SignUp />} />   
+*/
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Navbar />
+      <Routes>
+          <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
