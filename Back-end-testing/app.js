e9021dbca35484df90a497be78e47cfd64aa9d95
ZayaStar <Zayastarcaller@gmail.com>
2024-01-15 22:44:48 +0100

    modified some files for the back-end

--- Back-end-testing/app.js
+++ Back-end-testing/app.js
@@ -1,10 +1,13 @@
 import express, { json, urlencoded,} from 'express';
-import path, { join } from 'path';
+import path, { join, dirname } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import { fileURLToPath } from 'url';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 const app = express();
 
@@ -12,7 +15,7 @@ app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use((join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
