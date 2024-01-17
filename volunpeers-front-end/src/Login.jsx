873a6813de9b3ebd9b386f149c32ba1992c4d7da
3p0na <isa.alou@proton.me>
2024-01-17 14:35:57 -0500

    fix problem in Login.jsx

--- volunpeers-front-end/src/Login.jsx
+++ volunpeers-front-end/src/Login.jsx
@@ -2,3 +2,11 @@ import React, { useState } from 'react';
 import './App.css'; // Import the main styling, shared CSS file
 import './Login.css'; // Import the user profile styling
 
+
+
+const Login = () => {
+    const [selectedButtons, setSelectedButtons] = useState([]);
+  
+    };
+
+export default Login;
