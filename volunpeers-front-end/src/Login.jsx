815c6531a826615243ff159bc8f606ff177d9ac7
3p0na <isa.alou@proton.me>
2024-01-19 12:25:32 -0500

    added png and code (Login_Site)

--- volunpeers-front-end/src/Login.jsx
+++ volunpeers-front-end/src/Login.jsx
@@ -1,12 +1,56 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import './App.css'; // Import the main styling, shared CSS file
+// import './Login.css'; // Import the user profile styling
+
+
+
+// const Login = () => {
+//     const [selectedButtons, setSelectedButtons] = useState([]);
+  
+//     };
+
+    
+
+// export default Login;
+
+import React from 'react';
 import './App.css'; // Import the main styling, shared CSS file
 import './Login.css'; // Import the user profile styling
+import user_icon from "./components/Assets/person.png";
+import email_icon from "./components/Assets/email.png";
+import password_icon from "./components/Assets/password.png";
 
 
+export const Login = () => {
+  return (
+    <div className='container'>
+            <div className="underline">
+                <div className="inputs">
+                    <div className="input">
+                        <img src= {user_icon} alt="" />
+                        <input type="Username" />
+                    </div>
+                    <div className="input">
+                        <img src= {email_icon} alt="" />
+                        <input type="Email-Adresse" />
+                    </div>
+                    <div className="input">
+                        <img src= {password_icon} alt="" />
+                        <input type="Passwort" />
+                    </div>
+                    <div className="password_forgot">Passwort vergessen? <span>Hier klicken!</span></div>
+                </div>
+                <div className="submit_container">
+                    <div className="submit">Sign up</div>
+                    <div className="submit">Login</div>
+                </div>
+        </div>
 
-const Login = () => {
-    const [selectedButtons, setSelectedButtons] = useState([]);
-  
-    };
+    </div>
+  )
+}
 
 export default Login;
+
+
+
