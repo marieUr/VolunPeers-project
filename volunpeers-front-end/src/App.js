1ac0ff21ee513d000c5a1f74dc7e0db5aa8f32ff
sofiamaipa <sofiamaipa@gmail.com>
2024-01-28 21:14:03 +0100

    Fixed css code, added files into Profile folder

--- volunpeers-front-end/src/App.js
+++ volunpeers-front-end/src/App.js
@@ -6,26 +6,13 @@ import {
   BrowserRouter,
 } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
-import Profile from './Profile.jsx';
+import Profile from './components/Profile/Profile.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 import { Body } from './Pages/body.js';
 import { Footer } from './components/Footer.jsx';
 import { NotFound } from './Pages/NotFound.js';
 
-/*  Add one following lines above, Make the appropriate name changes to match your page.
-import Home from "./pages";
-import About from "./pages/about";
-import SignUp from "./pages/signup";
-import Contact from "./pages/contact";*/
-
-/* Then add one following lines inside <Routes> , with the names changed according to your files.
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/sign-up" element={<SignUp />} /> 
-          <Route path="/donations" element={<Donating />} />  
-          <Route path="/volunteering-projects" element={<VolProjects />} />
-*/
 
 function App() {
   return (
