346d48e1ce2578657f522a67e102d73e1a3738e5
marieUr <marieurbanska@gmail.com>
2024-01-29 14:58:54 +0100

    Changes to folders and files

--- volunpeers-front-end/src/components/Description/Description.jsx
+++ volunpeers-front-end/src/components/Description/Description.jsx
@@ -8,7 +8,41 @@ function Description() {
       <div className="Description-Rectangle">
         <div className="Description-Text">
           <h3>Why it matters to help our Planet</h3>
-          <p></p>
+          <p>
+            Embrace the Green Revolution! 🌱 Join us in making a difference by
+            volunteering to clean and preserve our planet. <br></br>Every action
+            counts, and together, we can create a healthier, cleaner world for
+            all.
+          </p>
+          <br></br>
+          <span>Why it matters:</span>
+          <ul>
+            <li>
+              &#8226; Environmental Impact: Reduce pollution and protect
+              ecosystems.
+            </li>
+            <li>
+              &#8226; Community Building: Connect with like-minded individuals
+              passionate about sustainability.
+            </li>
+            <li>
+              &#8226; Personal Growth: Gain new skills, experiences, and a sense
+              of fulfillment.
+            </li>
+          </ul>
+          <br></br>
+          <span>Join us </span>
+          <p>
+            on a journey to create a cleaner, greener world. Connect with
+            industry leaders, attend vibrant networking events, <br></br>
+            and choose from a myriad of projects that align with your passion
+            and skills.
+            <br></br>
+            Let your impact shine, and together, let's celebrate a planet
+            thriving in sustainability. <br></br>Be part of something
+            bigger.#VolunteerForChange and embark on a rewarding
+            #SustainabilityJourney! 💚🌏
+          </p>
         </div>
         <div className="Image-Box">
           <img src={CityImage} alt=""></img>
