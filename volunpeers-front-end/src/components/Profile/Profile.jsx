5b43bb03f85a80b5899e2fef9cf85d7a6aa2bdeb
sofiamaipa <sofiamaipa@gmail.com>
2024-02-06 13:46:21 +0100

    changed profile icon and styling. Added requests.

--- volunpeers-front-end/src/components/Profile/Profile.jsx
+++ volunpeers-front-end/src/components/Profile/Profile.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css'; // Import the main styling, shared CSS file
 import './Profile.css'; // Import the user profile styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faUser);
 
 const Profile = () => {
-  const [selectedButtons, setSelectedButtons] = useState([]);
-  const [checkedLists, setCheckedLists] = useState([[], [], []]);
+  const [selectedButtons, setSelectedButtons] = useState([]); // Store selected Interests
+  const [checkedLists, setCheckedLists] = useState([[], [], []]); // Store selected Availability
   const [totalSelected, setTotalSelected] = useState(0);
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [linkedIn, setLinkedIn] = useState('');
+  const [description, setDescription] = useState('');
+  const [languages, setLanguages] = useState('');
+  const [locations, setLocations] = useState('');
+  const [userId, setUserId] = useState('');
+
+  //const userId = '65ba70e4db270847a09b70a6'; //testing
 
   // Define checkLists array and isChecked function
   const checkLists = [
@@ -22,6 +41,86 @@ const Profile = () => {
       items: ["Building & Construction", "Community Development", "Culture & Arts", "Human & Women's Rights"],
     },
   ];
+
+  useEffect(() => {
+    // Fetch user information from an API endpoint
+    fetchUserInfo();
+  }, []); // Empty dependency array to run the effect only once when the component mounts
+
+  const fetchUserInfo = async () => {
+    try {
+      // Fetch userId from localStorage or wherever you store it
+      const storedUserId = localStorage.getItem('userId');
+      if (!storedUserId) {
+        console.error('User ID not found');
+        return;
+      }
+      setUserId(storedUserId);
+
+      // Fetch user information based on userId
+      const response = await fetch(`/api/profile/${storedUserId}`);
+      if (response.ok) {
+        const data = await response.json();
+        // Update state variables with fetched data
+        setFirstName(data.firstname);
+        setLastName(data.lastname);
+        setCity(data.city);
+        setCountry(data.country);
+        setPostalCode(data.postalCode);
+        setEmail(data.email);
+        setPhoneNumber(data.phoneNumber);
+        setLinkedIn(data.linkedIn);
+        setDescription(data.description);
+        setLanguages(data.languages);
+        setSelectedButtons(data.selectedButtons);
+        setLocations(data.locations);
+        setCheckedLists(data.checkLists);
+      } else {
+        console.error('Failed to fetch user information');
+      }
+    } catch (error) {
+      console.error('Error fetching user information:', error);
+    }
+  };
+  
+
+  const handleSaveProfile = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(`/api/profile/${userId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          firstname,
+          lastname,
+          city,
+          country,
+          postalCode,
+          email,
+          phoneNumber,
+          linkedIn,
+          description,
+          languages,
+          locations,
+          selectedButtons,
+          checkLists
+        }),
+      });
+      if (response.ok) {
+        alert('Profile updated successfully');
+      } else {
+        console.error('Failed to update profile');
+        alert('Failed to update profile');
+      }
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      alert('Error updating profile');
+    }
+  };
+
 
 
   const handleButtonClick = (buttonText) => {
@@ -82,42 +181,42 @@ const Profile = () => {
 
         <div className="white-box">
           <div className="profile-photo">
-            <img src="https://randomuser.me/api/portraits/men/33.jpg" alt="User's default photo" />{/* functionality to be added: when clicked, user selects a new picture */}
+          <FontAwesomeIcon class="profile-icon" icon={faUser} size="1x"/>{/* functionality to be added: when clicked, user selects a new picture */}
           </div>
 
           <div className="personal-info-content">
             <h3 class="title-h3">Personal Information</h3>
             <div className="info-field">
               <span class="span-info">First Name:</span>
-              <input type="text"  />
+              <input type="text" value={firstname} onChange={(e) => setFirstName(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">Last Name:</span>
-              <input type="text"  />
+              <input type="text" value={lastname} onChange={(e) => setLastName(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">City:</span>
-              <input type="text"  />
+              <input type="text" value={city} onChange={(e) => setCity(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">Country:</span>
-              <input type="text"  />
+              <input type="text" value={country} onChange={(e) => setCountry(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">Postal code:</span>
-              <input type="text"  />
+              <input type="text" value={postalCode} onChange={(e) => setPostalCode(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">Email:</span>
-              <input type="email"  />
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">Phone number:</span>
-              <input type="tel"  />
+              <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
             </div>
             <div className="info-field">
               <span class="span-info">LinkedIn profile:</span>
-              <input type="url"  />
+              <input type="url" value={linkedIn} onChange={(e) => setLinkedIn(e.target.value)}/>
             </div>
           </div>
         </div>
@@ -126,12 +225,11 @@ const Profile = () => {
           <h3 class="title-h3">About Me</h3>
           <div className="info-field answer-text">
             <span class="span-about">Describe yourself to an organization in 3-4 sentences:</span>
-            <textarea
-              />
+            <textarea value={description} onChange={(e) => setDescription(e.target.value)}/>
           </div>
           <div className="info-field answer-text">
             <span class="span-about">What languages do you speak? (Separate the languages you are fluent in with commas)</span>
-            <input type="text"  />
+            <input type="text" value={languages} onChange={(e) => setLanguages(e.target.value)}/>
           </div>
         </div>
 
@@ -142,27 +240,24 @@ const Profile = () => {
             <div className="availability-buttons">
               <button
                 className={selectedButtons.includes("Weekdays: Daytimes Monday-Friday") ? "clicked-button" : ""}
-                onClick={() => handleButtonClick("Weekdays: Daytimes Monday-Friday")}
-              >
+                onClick={() => handleButtonClick("Weekdays: Daytimes Monday-Friday")}>
                 Weekdays: Daytimes Monday-Friday
               </button>
               <button
                 className={selectedButtons.includes("Weeknights: Evenings Monday-Friday") ? "clicked-button" : ""}
-                onClick={() => handleButtonClick("Weeknights: Evenings Monday-Friday")}
-              >
+                onClick={() => handleButtonClick("Weeknights: Evenings Monday-Friday")}>
                 Weeknights: Evenings Monday-Friday
               </button>
               <button
                 className={selectedButtons.includes("Weekends: Anytime Saturday-Sunday") ? "clicked-button" : ""}
-                onClick={() => handleButtonClick("Weekends: Anytime Saturday-Sunday")}
-              >
+                onClick={() => handleButtonClick("Weekends: Anytime Saturday-Sunday")}>
                 Weekends: Anytime Saturday-Sunday
               </button>
             </div>
           </div>
           <div className="availability-field answer-text">
             <span class="span-availability">Where? (Separate your desired locations with commas) </span>
-            <input type="text"  />
+            <input type="text" value={locations} onChange={(e) => setLocations(e.target.value)}/>
           </div>
         </div>
 
@@ -190,7 +285,7 @@ const Profile = () => {
 
         </div>
         </div>
-        <button className="save-button">
+        <button className="save-button" onClick={handleSaveProfile}>
         Save
         </button>
         </div>
